484eca9c7bca24dd34b0aa5d9da0d7db86ea0a32
Zhaohan Dong <65422392+zhaohan-dong@users.noreply.github.com>
2024-07-15 20:00:11 +0000

    pgtbl: access

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,97 +6,109 @@
 #include "spinlock.h"
 #include "proc.h"
 
-uint64
-sys_exit(void)
+uint64 sys_exit(void)
 {
-  int n;
-  argint(0, &n);
-  exit(n);
-  return 0;  // not reached
+    int n;
+    argint(0, &n);
+    exit(n);
+    return 0; // not reached
 }
 
-uint64
-sys_getpid(void)
+uint64 sys_getpid(void) { return myproc()->pid; }
+
+uint64 sys_fork(void) { return fork(); }
+
+uint64 sys_wait(void)
 {
-  return myproc()->pid;
+    uint64 p;
+    argaddr(0, &p);
+    return wait(p);
 }
 
-uint64
-sys_fork(void)
+uint64 sys_sbrk(void)
 {
-  return fork();
+    uint64 addr;
+    int n;
+
+    argint(0, &n);
+    addr = myproc()->sz;
+    if (growproc(n) < 0)
+        return -1;
+    return addr;
 }
 
-uint64
-sys_wait(void)
+uint64 sys_sleep(void)
 {
-  uint64 p;
-  argaddr(0, &p);
-  return wait(p);
-}
+    int n;
+    uint ticks0;
 
-uint64
-sys_sbrk(void)
-{
-  uint64 addr;
-  int n;
-
-  argint(0, &n);
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
-  return addr;
-}
-
-uint64
-sys_sleep(void)
-{
-  int n;
-  uint ticks0;
-
-
-  argint(0, &n);
-  acquire(&tickslock);
-  ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
-      release(&tickslock);
-      return -1;
+    argint(0, &n);
+    acquire(&tickslock);
+    ticks0 = ticks;
+    while (ticks - ticks0 < n)
+    {
+        if (killed(myproc()))
+        {
+            release(&tickslock);
+            return -1;
+        }
+        sleep(&ticks, &tickslock);
     }
-    sleep(&ticks, &tickslock);
-  }
-  release(&tickslock);
-  return 0;
+    release(&tickslock);
+    return 0;
 }
-
 
 #ifdef LAB_PGTBL
-int
-sys_pgaccess(void)
+int sys_pgaccess(void)
 {
-  // lab pgtbl: your code here.
-  return 0;
+    // lab pgtbl: your code here.
+    uint64 addr;
+    int num_pages;
+    unsigned long int temp_abits = 0; // Access bits
+    int abits;
+
+    struct proc *p = myproc();
+
+    argaddr(0, &addr);
+    argint(1, &num_pages);
+    argint(2, &abits);
+
+    for (int i = 0; i < num_pages; i++)
+    {
+        uint64 current_addr = addr + i * PGSIZE;
+        pte_t *pte = walk(p->pagetable, current_addr, 0);
+        if (*pte & PTE_A)
+        {
+            // Accessed
+            temp_abits = temp_abits | (1 << i);
+        }
+        *pte = *pte & (~PTE_A);
+    }
+
+    if (copyout(p->pagetable, abits, (char *)&temp_abits, sizeof(temp_abits)) <
+        0)
+        return -1;
+
+    return 0;
 }
 #endif
 
-uint64
-sys_kill(void)
+uint64 sys_kill(void)
 {
-  int pid;
+    int pid;
 
-  argint(0, &pid);
-  return kill(pid);
+    argint(0, &pid);
+    return kill(pid);
 }
 
 // return how many clock tick interrupts have occurred
 // since start.
-uint64
-sys_uptime(void)
+uint64 sys_uptime(void)
 {
-  uint xticks;
+    uint xticks;
 
-  acquire(&tickslock);
-  xticks = ticks;
-  release(&tickslock);
-  return xticks;
+    acquire(&tickslock);
+    xticks = ticks;
+    release(&tickslock);
+    return xticks;
 }

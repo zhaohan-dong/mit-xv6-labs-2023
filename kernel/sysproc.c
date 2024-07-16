50d59e7b96877ea91c7d4c1beb6f61f4fa1f0d04
Zhaohan Dong <65422392+zhaohan-dong@users.noreply.github.com>
2024-07-16 23:09:26 +0000

    traps: alarm

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -80,3 +80,36 @@ uint64 sys_uptime(void)
     release(&tickslock);
     return xticks;
 }
+
+uint64 sys_sigalarm(void)
+{
+    int alarm_interval; // Alarm interval in ticks
+    uint64 handler_addr;
+
+    argint(0, &alarm_interval);
+    argaddr(1, &handler_addr);
+
+    printf("Called sigalarm - interval: %d %d\n", alarm_interval, handler_addr);
+
+    // Set flags on alarm
+    struct proc *p = myproc();
+
+    p->alarm_interval = alarm_interval;
+    p->alarm_handler_addr = handler_addr;
+
+    return 0;
+}
+
+uint64 sys_sigreturn(void)
+{
+    struct proc *p = myproc();
+
+    // Restore trapframe
+    memmove(p->trapframe, p->saved_trapframe, PGSIZE);
+    kfree(p->saved_trapframe);
+
+    p->alarm_ticks_passed = 0;
+    p->alarm_handler_running = 0;
+
+    return p->trapframe->a0;
+}
